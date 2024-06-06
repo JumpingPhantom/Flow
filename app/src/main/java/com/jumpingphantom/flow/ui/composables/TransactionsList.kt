f0e41d635a3bc501626d5e3f441af96fbd14a725
JumpingPhantom <104843253+jumpingphantom@users.noreply.github.com>
2024-06-06 17:19:58 +0300

    Renamed ExpensesList to TransactionsList and added basic add + display functionality

--- app/src/main/java/com/jumpingphantom/flow/ui/composables/TransactionsList.kt
+++ app/src/main/java/com/jumpingphantom/flow/ui/composables/TransactionsList.kt
@@ -1,11 +1,53 @@
 package com.jumpingphantom.flow.ui.composables
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.jumpingphantom.flow.util.Util
+import com.jumpingphantom.flow.viewmodel.TransactionViewmodel
+import kotlinx.coroutines.launch
 
 @Composable
-fun ExpensesList() {
-    LazyColumn {
+fun TransactionsList(viewmodel: TransactionViewmodel) {
+    val transactions by viewmodel.getTransactions().observeAsState(initial = emptyList())
+    val listState = rememberLazyListState()
 
+    LaunchedEffect(transactions.size) {
+        if (transactions.isNotEmpty()) {
+            launch {
+                listState.animateScrollToItem(transactions.size - 1)
+            }
+        }
     }
+
+    LazyColumn(state = listState, reverseLayout = true) {
+        items(transactions) { transaction ->
+            TransactionItem(transaction.isIncome, transaction.amount)
+        }
+    }
+}
+
+@Composable
+fun TransactionItem(type: Boolean, amount: Float) {
+    ListItem(
+        headlineContent = {
+            Text(text = if (type) "Income" else "Expense")
+        },
+        supportingContent = {
+            Text(Util.formatCurrency(amount))
+        },
+        trailingContent = {
+            Text("trailing")
+        },
+        modifier = Modifier.padding(8.dp)
+    )
 }
