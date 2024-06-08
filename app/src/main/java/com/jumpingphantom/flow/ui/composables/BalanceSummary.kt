3027f88af6784c599a23d04e46ac91d2945f5db7
JumpingPhantom <104843253+jumpingphantom@users.noreply.github.com>
2024-06-08 18:40:50 +0300

    Modified Card appearance

--- app/src/main/java/com/jumpingphantom/flow/ui/composables/BalanceSummary.kt
+++ app/src/main/java/com/jumpingphantom/flow/ui/composables/BalanceSummary.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,12 +43,16 @@ fun BalanceSummary(
         expenses.floatValue = it
     }
 
-    Card(modifier = Modifier.padding(horizontal = 8.dp)) {
-        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = Modifier.padding(horizontal = 16.dp)
+    ) {
+        Row(horizontalArrangement = Arrangement.SpaceAround, modifier = Modifier.fillMaxWidth()) {
             IncomeAndExpensesPieChart(income.floatValue, expenses.floatValue)
             IncomeAndExpenses(income, expenses)
         }
-        Row() {
+
+        Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
             CurrentBalance(income, expenses)
         }
     }
